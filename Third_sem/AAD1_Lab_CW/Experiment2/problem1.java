c303f892518a4f5f330f9f4a38d2f16157725022
KoustavBera <thekoustavofficial@gmail.com>
2024-09-15 23:59:17 +0530

    update

--- Third_sem/AAD1_Lab_CW/Experiment2/problem1.java
+++ Third_sem/AAD1_Lab_CW/Experiment2/problem1.java
@@ -18,30 +18,40 @@ public static void main(String[] args) {
 		for(int i=0;i<n;i++)
 		arr[i]=sc.nextInt();
          
-		contaguousSubArray(arr);
+		contiguousSubArray(arr);
  		sc.close();
 	}
 
-private static void contaguousSubArray(int[] arr) {
+	private static void contiguousSubArray(int[] arr) {
 
-	int maxLength = 0;
-	int currLen = 0;
-	int maxStartIndex = 0;
-	int startIndex=0;
-for(int i=1;i<arr.length;i++){
-	if(arr[i]==arr[i-1]+1){
-		currLen++;
-	}
-	else{
-		if(currLen > maxLength){
-			maxLength = currLen;
-			maxStartIndex = startIndex;
+		int maxLength = 0;        // To store the length of the longest contiguous subarray
+		int currLen = 1;          // Current subarray length
+		int maxStartIndex = 0;    // Index where the longest subarray starts
+		int startIndex = 0;       // Index where the current subarray starts
+
+		for (int i = 1; i < arr.length; i++) {
+			 // Check if the current element continues the contiguous sequence
+			 if (arr[i] == arr[i - 1] + 1) {
+				  currLen++;
+			 } else {
+				  // Check if the current subarray is the longest found so far
+				  if (currLen > maxLength) {
+						maxLength = currLen;
+						maxStartIndex = startIndex;
+				  }
+				  // Reset for the new subarray
+				  currLen = 1;
+				  startIndex = i;
+			 }
 		}
-		currLen = 1;
-		startIndex=i;
-	}
-}
-	System.out.println("Length of continuous subarray is "+maxLength);
-	System.out.println("that starts from "+maxStartIndex+" index");
-}
+
+		// After the loop, check if the last subarray was the longest
+		if (currLen > maxLength) {
+			 maxLength = currLen;
+			 maxStartIndex = startIndex;
+		}
+
+		System.out.println("Length of contiguous subarray is " + maxLength);
+		System.out.println("The subarray starts from index " + maxStartIndex);
+  }
 }

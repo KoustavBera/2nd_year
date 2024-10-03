3726823784f6782a283a0209e4c20073dc36d341
KoustavBera <thekoustavofficial@gmail.com>
2024-10-03 23:23:28 +0530

    update

--- Third_sem/AAD1_Lab_CW/Experiment3/problem8.java
+++ Third_sem/AAD1_Lab_CW/Experiment3/problem8.java
@@ -4,6 +4,21 @@ import java.util.Scanner;
  * problem8
  */
 public class problem8 {
+	/***  
+	 * Intuition/ How it works?
+	 *  
+	 *  1234 % 16 = 2
+	 *  77   % 16 = 12(D)
+	 *  4    % 16 = 4
+	 * 
+	 * Ans is 4D2
+	    1234 % 16 = 2 } our only task is to find the first remainder and add it to answer so ___ + hexChars[number%2]
+		 ----------------------------------------------------------------------------------
+	 *  77   % 16 = 12(D) } we expect that our DecToHex() func will give us 4D
+	 *  4    % 16 = 4     } so DecToHex(number/16)
+	 * 
+	 * 
+	 */
 
 	private static char[] hexChars = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 
